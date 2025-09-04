713f05750263f11cb4ffd7bd8a71a8515217a178
Devconte <romain.lcnte@gmail.com>
2025-09-04 11:17:26 +0200

    feat: add create andpost view

--- MiniBook/Models/Post.cs
+++ MiniBook/Models/Post.cs
@@ -1,13 +1,27 @@
-namespace MiniBook.Models;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
-public class Post
+namespace MiniBook.Models
 {
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public string Title { get; set; } = null!;
-    public string Content { get; set; } = null!;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public class Post
+    {
+        public int Id { get; set; }
 
-    public User User { get; set; } = null!;
-    public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Le titre est obligatoire")]
+        [StringLength(120)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Le contenu est obligatoire")]
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [ValidateNever] 
+        public User User { get; set; } = null!;
+
+        [ValidateNever]
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
 }
